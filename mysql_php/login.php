b5e10c92365c46019ac5d6b374bd7474c46d994a
quang200301 <quang.a24@student.passerellesnumeriques.org>
2023-03-17 17:30:09 +0700

    style

--- mysql_php/login.php
+++ mysql_php/login.php
@@ -25,20 +25,32 @@ if(isset($_POST['Login'])){
 ?>
 
 <html>
+    <link rel="stylesheet" href="stylelogin.css">
     <style>
         #name{
             margin-left:34px
         }
+        #login{
+            margin-left:14%
+    
+        }
     </style>
     <body>
+    <div class="content">
+
     <form action="" method="post" enctype="multipart/form-data">
+        <h1>FORM</h1>
     <label for="">Tên:</label>
     <input type="text" name="name" id="name"><br>
+    <!-- <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span> -->
     <label for="">mật khẩu:</label>
     <input type="text" name="mk"> <br><br>
-    <input type="submit" name="Login"  value="Login">
+    <input type="submit" name="Login"  id="login"value="Login">
+    <!-- <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> -->
 
 </form>
+</div>
         
     </body>
 </html>
